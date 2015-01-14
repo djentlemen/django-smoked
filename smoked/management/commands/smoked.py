c488a9cd03d10919abf17e5c0c81181cd237ba65
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2015-01-14 21:35:43 +0100

    Added dry_run mode which only counts smoke tests

--- smoked/management/commands/smoked.py
+++ smoked/management/commands/smoked.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 from __future__ import absolute_import, unicode_literals
+from optparse import make_option
 
 import time
 from django.core.management.base import NoArgsCommand
+from smoked import default_registry
 
 from smoked.runner import run_tests
 
@@ -18,8 +20,20 @@ Time: {time}
 
 
 class Command(NoArgsCommand):
+    option_list = NoArgsCommand.option_list + (
+        make_option(
+            '-n', '--dry-run', dest='dry_run',
+            action='store_true', default=False,
+            help="Only collect test, don't execute them."),
+        )
+
     def handle_noargs(self, **options):
         start_time = time.time()
+
+        if options.get('dry_run'):
+            count = len(default_registry.tests)
+            self.stdout.write('{0} smoke test(s) could be run'.format(count))
+            return
 
         success = failure = 0
         for result in run_tests():
--- tests/test_management.py
+++ tests/test_management.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
 from django.core.management import call_command
 from django.utils.six import StringIO
 from smoked import register
@@ -23,3 +26,25 @@ def test_verbose_command(valid_test, invalid_test):
     assert line('Total: 5') in output
     assert line('Success: 3') in output
     assert line('Failure: 2') in output
+
+
+def test_smoked_dry_run(valid_test, invalid_test):
+    assert not valid_test.called
+    assert not invalid_test.called
+
+    # Generate test suit
+    register(valid_test)
+    register(valid_test)
+    register(invalid_test)
+    register(valid_test)
+    register(invalid_test)
+
+    stdout = StringIO()
+    call_command('smoked', dry_run=True, stdout=stdout)
+
+    stdout.seek(0)
+    output = stdout.readlines()
+
+    assert not valid_test.called
+    assert not invalid_test.called
+    assert line('5 smoke test(s) could be run') in output
