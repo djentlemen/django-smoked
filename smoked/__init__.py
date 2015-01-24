8ee891b8c0b1974fc49194d289d4d575d28e00c3
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2015-01-24 16:47:18 +0100

    Don't import Django when installing package.

--- smoked/__init__.py
+++ smoked/__init__.py
@@ -1,9 +1,6 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-import django
-from django.conf import settings
-
 from .registry import Registry
 
 __version__ = '0.1-alpha.1'
@@ -15,7 +12,14 @@ register = default_registry.register
 
 default_app_config = 'smoked.apps.SmokedApp'
 
-if settings.configured and django.VERSION[1] < 7:
-    # Dj<1.7 doesn't have App registry
-    from .loader import load_test_module
-    load_test_module()
+try:
+    import django
+except ImportError:
+    " Probably running `python setup.py` which imports __version__. Ignore it."
+else:
+    from django.conf import settings
+
+    if settings.configured and django.VERSION[1] < 7:
+        # Dj<1.7 doesn't have App registry
+        from .loader import load_test_module
+        load_test_module()
