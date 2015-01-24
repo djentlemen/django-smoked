25e8c3d68d0086af0c57e54929b00050d33bf108
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2015-01-24 12:58:02 +0100

    Fixed import paths.

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 
 SMOKE_TESTS = 'tests.functional.simple'
 
-ROOT_URLCONF = 'functional.urls'
+ROOT_URLCONF = 'tests.functional.urls'
 INSTALLED_APPS = ['smoked']
 SECRET_KEY = 'must not be empty'
