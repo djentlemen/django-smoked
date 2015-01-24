7c858b0fed22b0fe4f86869dbacfd8e78f475ded
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2015-01-24 12:48:51 +0100

    Fix import path to testing smoke tests module.

--- tests/settings.py
+++ tests/settings.py
@@ -1,4 +1,7 @@
-SMOKE_TESTS = 'smoke_tests.simple'
+# coding: utf-8
+from __future__ import unicode_literals
+
+SMOKE_TESTS = 'tests.smoke_tests.simple'
 
 ROOT_URLCONF = 'smoke_tests.urls'
 INSTALLED_APPS = ['smoked']
---
+++ tests/smoke_tests/simple.py
@@ -0,0 +1,6 @@
+from smoked import register
+
+
+@register
+def optimistic_test():
+    return True
