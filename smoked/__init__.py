a6769bfe4331a030f5a842ce6ca7e8659585b14d
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2015-01-24 13:37:03 +0100

    Fixed installing package with Dj<17

--- smoked/__init__.py
+++ smoked/__init__.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 import django
+from django.conf import settings
+
 from .registry import Registry
 
 __version__ = '0.1-alpha.1'
@@ -13,7 +15,7 @@ register = default_registry.register
 
 default_app_config = 'smoked.apps.SmokedApp'
 
-if django.VERSION[1] < 7:
+if settings.configured and django.VERSION[1] < 7:
     # Dj<1.7 doesn't have App registry
     from .loader import load_test_module
     load_test_module()
